272869b16f41413c63acbca1ea107f1fac503ef6
IveTou <ive_tourinho@hotmail.com>
2019-04-06 02:48:18 -0300

    Basic structure working

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,33 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <Layout/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Layout from './components/Layout.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Layout
   }
 }
 </script>
 
 <style>
-#app {
+body {
+  margin: 0;
+}
+.app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: lightgrey;
 }
 </style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="layout">
+    <md-card>
+      <md-card-header>
+        Hello
+      </md-card-header>
+    </md-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
