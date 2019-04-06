4200fe5336a46dec1f4daa748669ab4fdab9f4e1
IveTou <ive_tourinho@hotmail.com>
2019-04-06 02:15:04 -0300

    Vue Material

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueMaterial)
 
 new Vue({
   render: h => h(App),
